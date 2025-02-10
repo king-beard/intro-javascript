13393e5101fff086943807a16a17702e92c4897c
king-beard <chucho_herrera10@icloud.com>
2025-02-10 11:29:04 -0600

    object-literal

---
+++ src/bases/object-literal.js
@@ -0,0 +1,19 @@
+// Objetos Literales
+
+const people = {
+    name : 'Jesus',
+    lastname : 'Herrera',
+    age : 27,
+    address : {
+        city : 'Qro',
+        postalCode : 76708,
+        lat : 46.23423432,
+        lng : 54.5454,
+    }
+}
+
+const people2 = { ...people }
+people2.name = 'Kakaroto'
+
+console.log(people)
+console.log(people2)
