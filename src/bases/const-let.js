5c4ccf5ff1750cc9c81c25f5979bbfe43fd3c018
kingbeard <chucho_herrera10@icloud.com>
2025-02-10 10:47:00 -0600

    template-string

---
+++ src/bases/const-let.js
@@ -0,0 +1,17 @@
+// variables y constantes
+
+const name = 'Jesus'
+const lastname = "Herrera"
+
+let value = 5
+value = 4
+
+console.log(name, lastname, value)
+
+// var no se debe de usar 
+if(true){
+    const name = 'Kakaroto'
+    console.log(name)
+}
+
+console.log(value)
---
+++ src/bases/template-string.js
@@ -0,0 +1,12 @@
+// Template string
+
+const name = 'Kakaroto'
+const lastname = 'Herrera'
+
+const fullname = `${name} ${lastname}`
+ 
+console.log(fullname)
+
+const getSaludo = ( fullname ) =>  `Hello ${fullname}!!!`;
+
+console.log(`Este es un texto: ${getSaludo(fullname)}`)
