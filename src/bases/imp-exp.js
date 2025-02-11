a52756e59515940dd2d70d454ee753983dc16920
king-beard <chucho_herrera10@icloud.com>
2025-02-11 13:37:11 -0600

    import and export

---
+++ src/bases/imp-exp.js
@@ -0,0 +1,8 @@
+import { heroes } from '../data/heroes'
+
+const getHeroeById = (id) => heroes.find(heroe => heroe.id == id);
+const getHeroeByOwner = (owner) => heroes.filter(heroe => heroe.owner == owner);
+
+console.log( getHeroeById(2) )
+console.log( getHeroeByOwner('DC') )
+console.log( getHeroeByOwner('Marvel') )
