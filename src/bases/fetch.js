1c3bfd0e1f07521617c049a8c64a249cc2822695
king-beard <chucho_herrera10@icloud.com>
2025-02-11 14:34:33 -0600

    fetch

---
+++ src/bases/fetch.js
@@ -0,0 +1,14 @@
+const apiKey = 'wmTOxy7L04iD1a1xlnS7YCtEzi532F2o'
+
+const request = fetch(`https://api.giphy.com/v1/gifs/random?api_key=${apiKey}`)
+
+request
+  .then(response => response.json())
+  .then(({ data }) => {
+    const { url } = data.images.original
+    const img = document.createElement('img')
+    img.src = url
+
+    document.body.append(img)
+  })
+  .catch(console.warn)
