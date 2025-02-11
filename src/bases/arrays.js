83d93b6905a4191061a8a74357cafe06963f0eb5
king-beard <chucho_herrera10@icloud.com>
2025-02-10 20:46:26 -0600

    arrays

---
+++ src/bases/arrays.js
@@ -0,0 +1,13 @@
+// Arreglos en JS
+
+const array = [1,2,3,4];
+
+let array2 = [ ...array, 5 ];
+
+const array3 = array2.map(function(i) {
+    return i * 2;
+});
+
+console.log(array)
+console.log(array2)
+console.log(array3)
